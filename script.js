c3ca4e2bbb9e769ef306ad189e3ce3be33a4d10b
KostiantynPolishko <polxs_wp31@student.itstep.org>
2024-06-03 13:10:41 +0300

    2 Edited function createCardDog

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', (e) => {
     console.log('Start');
   
-    createCardDog('main', 'flex_row');
-
+    $('main.flex_row')[0].append(createCardDog('dog1'));
 })
 
-const createCardDog = (element, className) => {
-    $(`${element}.${className}`).append($('<div>').attr('id', 'dog1').addClass('card_dog'));
-    $('div#dog1').append($('<button>').attr({'type':'name', 'button':'load'}).text('Load Img'));
-    $('div#dog1').append($('<div>').addClass('img_dog'));
+const createCardDog = (id) => {
+
+    let card = $('<div>').attr('id', `${id}`).addClass('card_dog')[0];
+    $(card).append($('<button>').attr({'type':'name', 'button':'load'}).text('Load Img'));
+    $(card).append($('<div>').addClass('img_dog'));
+
+    return card;
 }
